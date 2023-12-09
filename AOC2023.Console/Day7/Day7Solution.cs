919e7ec6ba9fe48539a2dd1da257b1bbfa762c44
Shan Richardson <zerolake@tuta.io>
2023-12-09 17:38:31 -0500

    Added solution in progress for Day 7 part 1

--- AOC2023.Console/Day7/Day7Solution.cs
+++ AOC2023.Console/Day7/Day7Solution.cs
@@ -5,9 +5,97 @@ namespace AOC2023.Console.Day7;
 public class Day7Solution : ISolution
 {
     public int Day => 7;
+
+    public static readonly List<char> cardOrder = new List<char>()
+    {
+        '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'
+    };
+
+    public enum HandType
+    {
+        HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind
+    };
+
     public string Solve(string[] input)
     {
-
-        return "Not yet solved";
+        input = new string[]
+        {
+            "32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483"
+        };
+        
+        return SolvePart1(input);
     }
+
+    private string SolvePart1(string[] input)
+    {
+        Dictionary<HandType, List<Hand>> hands = new Dictionary<HandType, List<Hand>>();
+
+        foreach (var line in input)
+        {
+            var hand = new Hand(line.Split(" ")[0], int.Parse(line.Split(" ")[1]));
+            if (!hands.ContainsKey(hand.type)) hands[hand.type] = new List<Hand>();
+            hands[hand.type].Add(hand);
+            
+            System.Console.WriteLine($"Hand: {hand.cards} / Type: {hand.type} / Bet: {hand.bet}");
+        }
+        
+        return $"s";
+    }
+    
+    private struct Hand
+    {
+        public string cards;
+        public HandType type;
+        public int bet;
+        public int[] highCardValues = new int[5];
+
+        public Hand(string cards, int bet)
+        {
+            this.cards = cards;
+            this.bet = bet;
+            DetermineType();
+            DetermineHighCardValue();
+        }
+
+        private void DetermineType()
+        {
+            var cardCount = CountCards();
+            type = cardCount switch
+            {
+                (1, 5) => HandType.FiveOfAKind,
+                (2, 4) => HandType.FourOfAKind,
+                (2, 3) => HandType.FullHouse,
+                (3, 3) => HandType.ThreeOfAKind,
+                (3, 2) => HandType.TwoPair,
+                (4, 2) => HandType.OnePair,
+                _ => HandType.HighCard
+            };
+        }
+        
+        private (int, int) CountCards()
+        {
+            var data = new Dictionary<char, int>();
+
+            foreach (var card in cards)
+            {
+                if (!data.ContainsKey(card)) data[card] = 1;
+                else data[card]++;
+            }
+
+            return (data.Keys.Count, data.Values.Max());
+        }
+
+        private void DetermineHighCardValue()
+        {
+            for (var i = 0; i < cards.Length; i++)
+            {
+                highCardValues[i] = cardOrder.IndexOf(cards[i]);
+            }
+        }
+    }
+    
 }
